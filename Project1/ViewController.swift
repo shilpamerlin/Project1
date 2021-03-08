ada3d9b82eb5ca92da96b9b1f8cbae7d679db7d8
Shilpa <shilpamjoy@gmail.com>
2021-03-08 13:28:41 -0500

    Done with project as well as challenges

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
+        navigationController?.navigationBar.prefersLargeTitles = true
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Storm Viewer"
+        let fm = FileManager.default // FileManager.default. This is a data type that lets us work with the filesystem
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted()
+       
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.font = UIFont.init(name: "Helvetica", size: 20)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            vc.totalPictures = pictures.count
+           // vc.imageIndex = find(searchValue: pictures[indexPath.row], pictureArray: pictures)
+            vc.imageIndex = indexPath.row
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
     }
 
+    /*func find(searchValue: String, pictureArray: [String]) -> Int{
+        
+        for(index, value) in pictures.enumerated() {
+            if value == searchValue {
+                return index
+            }
+            
+        }
+return 0
+    }*/
 
 }
 
